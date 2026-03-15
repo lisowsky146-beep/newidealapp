c72c5c4763441cc8da6f1057cb70427e6fb13ed2
lisowsky146-beep <lisowsky146@gmail.com>
2026-03-16 00:19:23 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,4 +100,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    implementation("androidx.compose.material3:material3:1.2.1")
 }
